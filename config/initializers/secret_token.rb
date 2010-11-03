d296eecae7fa971ccf723a618ef4b97a21005831
Alex Stupka <astupka21@yahoo.com>
2010-11-03 13:41:53 -0400

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogViewer::Application.config.secret_token = 'e818a698da2da4afb8118b66a675c28b2fc71307a15a6dbcda7eb0576b2242eb62e0ba8b62e16386de36981777d2558727415b1d423e0fcc804e0b7fec0c3679'
