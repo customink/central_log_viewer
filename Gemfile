40967462221de848ecae821368e7541b411147b1
Alex Stupka <astupka21@yahoo.com>
2010-11-16 15:58:00 -0500

    [Fixes #6440041] Finished initial working mockup

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.0'
 
 # gem 'sqlite3-ruby', :require => 'sqlite3'
 gem "central_logger"
+gem "haml"
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ LogViewer::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :log, :only => [:show, :index]
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +49,7 @@ LogViewer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "log#index"
 
   # See how all your routes lay out with "rake routes"
 
