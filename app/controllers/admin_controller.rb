471505072d833cd279eb5eb74b912cac724dfc73
Pratik Khadloya <tispratik@gmail.com>
2011-08-07 17:23:32 -0700

    Implemented changes suggested by chmurph2 though not quite done with the http digest testing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,13 @@
 class AdminController < ApplicationController
 
-  USERS = { APP_CONFIG["username"] => APP_CONFIG["password"] }
-
-  before_filter :authenticate if APP_CONFIG["is_enabled"]
+  before_filter :authenticate if APP_CONFIG["authenticate"]
 
   private
 
   def authenticate
+    users = { APP_CONFIG["username"] => APP_CONFIG["password"] }
     authenticate_or_request_with_http_digest do |username|
-      USERS[username]
+      users[username]
     end
   end
 end
--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,6 +1,6 @@
 begin
-  APP_CONFIG = YAML::load(ERB.new(IO.read(File.join(RAILS_ROOT, 'config', 'auth.yml'))).result)[RAILS_ENV]
+  APP_CONFIG = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'auth.yml'))).result)[Rails.env]
 rescue Exception => e
   puts "Central Log Viewer: HTTP Authentication not enabled"
-  APP_CONFIG = { "is_enabled" => false }
+  APP_CONFIG = { "authenticate" => false }
 end
